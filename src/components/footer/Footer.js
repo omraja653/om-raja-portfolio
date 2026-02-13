f65e7355c23c228807b8f48dcf1f76e1e1633e32
OM RAJA <rajaom653@gmail.com>
2026-02-13 13:57:58 +0530

    Format code and update footer class names

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,19 +1,20 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Footer.scss";
-import {Fade} from "react-reveal";
+import { Fade } from "react-reveal";
 import emoji from "react-easy-emoji";
 import StyleContext from "../../contexts/StyleContext";
 
 export default function Footer() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
+
   return (
     <Fade bottom duration={1000} distance="5px">
-      <div className="footer-div">
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
+      <div className={isDark ? "footer-div dark-footer" : "footer-div"}>
+        <p className="footer-text">
           {emoji(" ")}
         </p>
-        <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
-          {" "}
+
+        <p className="footer-text">
           <a
             href="https://www.linkedin.com/in/om-raja-84850b240/"
             target="_blank"
